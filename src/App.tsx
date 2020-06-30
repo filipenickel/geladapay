fd7107d269c9fc30d3bff15d8dbe0e3758002546
Aran Leite <hyoretsu@gmail.com>
2020-06-30 19:44:34 -0300

    Added fonts, svg import packages and fixed assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import { View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { AppLoading } from 'expo';
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { Lato_400Regular, Lato_700Bold, Lato_900Black, useFonts } from '@expo-google-fonts/lato';
 
 import Routes from './routes';
 
-const App: React.FC = () => (
-  <NavigationContainer>
-    <StatusBar style="auto" />
-    <View style={{ flex: 1, backgroundColor: '#fff' }}>
-      <Routes />
-    </View>
-  </NavigationContainer>
-);
+const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Lato_400Regular,
+    Lato_700Bold,
+    Lato_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#e5e5e5' }}>
+        <Routes />
+      </SafeAreaView>
+    </NavigationContainer>
+  );
+};
 
 export default App;

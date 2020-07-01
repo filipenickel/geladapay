406bd9fe426ae3ca7faa6235c1a30fd227fef565
Aran Leite <hyoretsu@gmail.com>
2020-07-01 04:02:49 -0300

    Welcome page refactoring

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,70 +1,75 @@
 import React from 'react';
 import SvgUri from 'react-native-svg-uri';
 import Icon from 'react-native-vector-icons/Feather';
+
+import { TouchableOpacity } from 'react-native';
+import HavingFun from '../../assets/having_fun.svg';
+
 import {
-  Body,
-  Title,
-  Button,
+  Container,
   Header,
-  Span,
-  SubTitle,
-  ButtonSpan,
-  DivButtonFooter,
-  DivImg,
-  BackgroundButton,
-  SpanButton,
-  DivIconsSocial,
-  ButtonIconSocial,
-  DivGroup,
+  Title,
+  BlackTitle,
+  OrangeTitle,
+  Subtitle,
+  Body,
+  SignInButton,
+  ButtonBackground,
+  ButtonText,
+  AccountLinks,
+  SignupText,
+  AccountLinksText,
+  SocialNetworks,
+  SNSButtons,
 } from './styles';
 
 const Welcome: React.FC = () => {
   return (
-    <Body>
+    <Container>
       <Header>
-        <Title>Bem-Vindo ao Choppy</Title>
-        <SubTitle>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sem. </SubTitle>
+        <Title>
+          <BlackTitle>Bem-Vindo ao </BlackTitle>
+          <OrangeTitle>Choppy</OrangeTitle>
+        </Title>
+
+        <Subtitle>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sem. </Subtitle>
       </Header>
 
-      <DivImg>
-        <SvgUri width="315" height="315" source={require('../../assets/having_fun.svg')} />
-      </DivImg>
+      <Body>
+        <SvgUri width="315" height="196" source={HavingFun} />
+      </Body>
 
-      <DivGroup>
-        <BackgroundButton colors={['#BA0C2F', '#FF6A13']} start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }}>
-          <Button onPress={() => {}}>
-            <SpanButton>ENTRAR</SpanButton>
-            <Icon name="arrow-right" color="white" size={18} />
-          </Button>
-        </BackgroundButton>
+      <SignInButton activeOpacity={0.7}>
+        <ButtonBackground colors={['#BA0C2F', '#FF6A13']} start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }}>
+          <ButtonText>Entrar</ButtonText>
+          <Icon name="arrow-right" color="#fff" size={18} />
+        </ButtonBackground>
+      </SignInButton>
 
-        <DivButtonFooter>
-          <ButtonSpan onPress={() => {}}>
-            <Span>Cadastrar-se </Span>
-          </ButtonSpan>
+      <AccountLinks>
+        <TouchableOpacity>
+          <SignupText>Cadastrar-se</SignupText>
+        </TouchableOpacity>
 
-          <Span>|</Span>
+        <AccountLinksText> | </AccountLinksText>
 
-          <ButtonSpan onPress={() => {}}>
-            <Span> Esqueci a minha senha</Span>
-          </ButtonSpan>
-        </DivButtonFooter>
-      </DivGroup>
+        <TouchableOpacity>
+          <AccountLinksText>Esqueci a minha senha</AccountLinksText>
+        </TouchableOpacity>
+      </AccountLinks>
 
-      <DivIconsSocial>
-        <ButtonIconSocial>
-          <Icon name="facebook" color="black" size={24} />
-        </ButtonIconSocial>
-
-        <ButtonIconSocial>
-          <Icon name="instagram" color="black" size={24} />
-        </ButtonIconSocial>
-
-        <ButtonIconSocial>
-          <Icon name="twitter" color="black" size={24} />
-        </ButtonIconSocial>
-      </DivIconsSocial>
-    </Body>
+      <SocialNetworks>
+        <SNSButtons>
+          <Icon name="facebook" color="#000" size={24} />
+        </SNSButtons>
+        <SNSButtons>
+          <Icon name="instagram" color="#000" size={24} />
+        </SNSButtons>
+        <SNSButtons>
+          <Icon name="twitter" color="#000" size={24} />
+        </SNSButtons>
+      </SocialNetworks>
+    </Container>
   );
 };
 
--- src/pages/Welcome/styles.ts
+++ src/pages/Welcome/styles.ts
@@ -1,81 +1,98 @@
 import styled from 'styled-components/native';
 import { LinearGradient } from 'expo-linear-gradient';
 
-export const Body = styled.View`
+export const Container = styled.View`
   flex: 1;
 `;
 
 export const Header = styled.View`
-  align-items: center;
   flex-direction: column;
-  margin-top: 25px;
-`;
-
-export const Title = styled.Text`
-  color: #ff6a13;
-  font-size: 30px;
-  font-family: 'Lato_900Black';
-`;
-
-export const SubTitle = styled.Text`
-  margin-top: 25px;
-  font-size: 18px;
-  font-family: 'Lato_400Regular';
-  line-height: 22px;
-  width: 297px;
-  height: 44px;
   align-items: center;
+  align-self: center;
+  width: 80%;
+`;
+
+export const Title = styled.View`
+  flex-direction: row;
+  margin: 60px 0 45px;
+`;
+
+export const BlackTitle = styled.Text`
+  font-family: 'Lato_900Black';
+  font-size: 30px;
+  color: #333;
+`;
+
+export const OrangeTitle = styled.Text`
+  font-family: 'Lato_900Black';
+  font-size: 30px;
+  color: #ff6a13;
+`;
+
+export const Subtitle = styled.Text`
+  font-family: 'Lato_400Regular';
+  font-size: 18px;
+  color: #333;
+  line-height: 22px;
   text-align: center;
 `;
 
-export const SpanButton = styled.Text`
-  color: white;
-  font-family: 'Lato_700Bold';
+export const Body = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
 `;
 
-export const DivImg = styled.View`
-  align-items: center;
+export const SignInButton = styled.TouchableOpacity`
+  width: 316px;
+  height: 48px;
+
+  margin: 0 auto;
+  box-shadow: 0px 3px 9px rgba(0, 0, 0, 0.18);
 `;
 
-export const Button = styled.TouchableOpacity`
+export const ButtonBackground = styled(LinearGradient)`
+  flex: 1;
+  flex-direction: row;
   align-items: center;
+  justify-content: center;
   border-radius: 30px;
+`;
+
+export const ButtonText = styled.Text`
+  font-family: 'Lato_700Bold';
+  font-size: 18px;
+  color: #fff;
+  text-transform: uppercase;
+  margin-right: 3px;
+`;
+
+export const AccountLinks = styled.View`
   flex-direction: row;
   justify-content: center;
-  box-shadow: 0px 3px 9px;
+  margin: 24px 0 30px;
 `;
 
-export const ButtonSpan = styled.TouchableOpacity`
-  font-family: 'Lato_700Bold';
-`;
-
-export const Span = styled.Text`
+export const SignupText = styled.Text`
   font-family: 'Lato_700Bold';
   font-size: 18px;
 `;
-export const DivGroup = styled.View``;
-export const BackgroundButton = styled(LinearGradient)`
-  align-items: center;
-  justify-content: center;
-  border-radius: 30px;
-  width: 316px;
-  height: 48px;
-  margin: 20px auto 30px auto;
+
+export const AccountLinksText = styled.Text`
+  font-family: 'Lato_400Regular';
+  font-size: 18px;
 `;
 
-export const DivButtonFooter = styled.View`
+export const SocialNetworks = styled.View`
   flex-direction: row;
-  justify-content: center;
+  justify-content: space-between;
+  width: 195px;
+  margin: 0 auto 31px;
 `;
-export const DivIconsSocial = styled.View`
-  flex-direction: row;
-  justify-content: center;
-  margin-top: 25px;
-`;
-export const ButtonIconSocial = styled.TouchableOpacity`
+
+export const SNSButtons = styled.TouchableOpacity`
   background: white;
-  margin: 0 5px;
-  padding: 15px;
-  border-radius: 70px;
-  box-shadow: 0px 3px 9px;
+  padding: 10px;
+  border-radius: 22px;
+  box-shadow: 0px 3px 9px rgba(0, 0, 0, 0.18);
 `;

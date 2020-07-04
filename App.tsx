40d05b6c56add2beb0cc25ec703fb0eca8f93775
Aran Leite <hyoretsu@gmail.com>
2020-07-04 13:16:51 -0300

    Fixed underlying Expo error

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { Lato_400Regular, Lato_700Bold, Lato_900Black, useFonts } from '@expo-google-fonts/lato';
 
-import Routes from './routes';
+import Routes from './src/routes';
 
 const App: React.FC = () => {
   const [fontsLoaded] = useFonts({
